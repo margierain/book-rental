f1fc7ba176a9c9921544c1acaf17e45a2419b47a
margierain <margaretrain.mo@gmail.com>
2020-02-02 03:21:00 +0300

    adds cart functionality

--- src/components/book/book.js
+++ src/components/book/book.js
@@ -1,16 +1,33 @@
 import React, { useContext } from "react";
 import { CartContext } from "../cart/cartContext";
-import { useForm, FormContext } from 'react-hook-form'
+import { useForm, FormContext } from 'react-hook-form';
+
+const allBooks = [];
+let book = {};
 
 export const Book = props => {
   let [cart, setCart] = useContext(CartContext);
 
-
   const addToCart = (data) => {
-    const { duration } = data;
-    const book = { type: props.type, id: props.id, duration: duration };
+    const { duration, bookNum } = data;
+    let duration1 = parseFloat(duration);
+    let bookNum1 = parseFloat(bookNum)
 
-    cart = currentState => [...currentState, book];
+  if( allBooks.length >= 1) {
+    allBooks.map((book) => {
+      if(book.id === props.id && (parseFloat(book.duration) !== parseFloat(duration))) {
+        duration1  += parseFloat(book.duration)
+      }
+
+      if(props.id === book.id && (parseFloat(book.bookNum) !== parseFloat(bookNum))) {
+        bookNum1 += parseFloat(book.bookNum)
+      }
+    })
+  }
+  book = Object.assign({}, { type: props.type, id: props.id, duration: duration1, bookNum:bookNum1 });
+  allBooks.push(book);
+
+    cart = currentState => currentState.concat(allBooks)
     setCart(cart);
   };
 
@@ -22,7 +39,10 @@ export const Book = props => {
       <h2>{props.type}</h2>
       <FormContext {...methods}>
         <form onSubmit={handleSubmit(addToCart)}>
-             <input type="number" name="duration" placeholder="Duration"  ref={register({ required: true })}/>
+           <label>Number of rental days</label>
+            <input type="number" name="duration" placeholder="Duration"  ref={register({ required: true })}/>
+            <label>Number of books your renting</label>
+            <input type="number" name="bookNum" placeholder="Book Number"  ref={register({ required: true })}/>
             <button type="submit" >Add to cart</button>
         </form>
       </FormContext>
